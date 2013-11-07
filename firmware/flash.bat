ffb5919aff89c670b6d0bb2a28d9fb9fa46ba93d
Leo <leomichine@gmail.com>
2013-11-07 23:56:39 +0400

    Added win cmd batch for flash firmware to keyboard

---
+++ firmware/flash.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Atmel\Flip 3.4.7\bin\batchisp.exe" -device AT90USB162 -hardware usb -operation erase f memory flash blankcheck loadbuffer catboard.hex program verify start noreset 0
+pause
