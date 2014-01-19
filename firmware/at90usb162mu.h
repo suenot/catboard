5e8a862829ca3aa56225a6cdd1a8f1d34ddc6476
Leo <leomichine@gmail.com>
2014-01-20 00:41:30 +0400

    fix for game layer

--- firmware/at90usb162mu.h
+++ firmware/at90usb162mu.h
@@ -158,7 +158,7 @@ const uint8_t layer_fnlock[KEYS] = {
 const uint8_t layer_fnlock[KEYS] = { //for gaming
 	//ROW1				ROW2			ROW3			ROW4			ROW5
 	KEY_TILDE,		KEY_TAB,			KEY_RCTRL,		KEY_ALT_TAB,	KEY_ESC,		// COL1
-	KEY_1,			KEY_Q,				KEY_A,			KEY_LGUI,		KEY_SHIFT,		// COL2
+	KEY_1,			KEY_Q,				KEY_A,			KEY_LGUI,		KEY_RSHIFT,		// COL2
 	KEY_2,			KEY_W,				KEY_S,			KEY_X,			KEY_Z,			// COL3
 	KEY_3,			KEY_E,				KEY_D,			KEY_C,			KEY_LALT,		// COL4
 	KEY_4,			KEY_R,				KEY_F,			KEY_V,			KEY_LCTRL,		// COL5
@@ -167,6 +167,6 @@ const uint8_t layer_fnlock[KEYS] = { //for gaming
 	KEY_7,			KEY_U,				KEY_J,			KEY_M,			KEY_FN,			// COL8
 	KEY_8,			KEY_I,				KEY_K,			KEY_COMMA,		KEY_RALT,		// COL9
 	KEY_9,			KEY_O,				KEY_L,			KEY_PERIOD,		KEY_SLASH,		// COL10
-	KEY_0,			KEY_P,				KEY_SEMICOLON,	KEY_BACKSLASH,	KEY_SHIFT,		// COL11
+	KEY_0,			KEY_P,				KEY_SEMICOLON,	KEY_BACKSLASH,	KEY_LSHIFT,		// COL11
 	KEY_MINUS,		KEY_LEFT_BRACE,		KEY_QUOTE,		KEY_RIGHT_BRACE,KEY_EQUAL		// COL12
 };
